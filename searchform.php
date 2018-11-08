e432c25c6128f36f8454c7ee841fcc5a3eb0e277
aktarzaman <aktarzaman84@gmail.com>
2018-11-08 17:24:31 +0600

    Deleted Old Theme

--- searchform.php
+++
@@ -1,4 +0,0 @@
-<form role="search" method="get" class="input-group" action="<?php echo home_url('/'); ?>">
-	<input type="search" class="form-control" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s" title="Search" />
-	<input type="submit" value="search">
-</form>
